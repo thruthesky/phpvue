1bf9d42172152cddba18bbe74e4de641fb158ea9
JAEHO SONG <thruthesky@gmail.com>
2021-05-01 20:49:07 +0900

    remove seo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+function domRemove(id) {
+    document.getElementById(id).outerHTML = "";
+}
 /**
  * Vue 앱을 생성하고, root instance 를 `#app` 으로 mount 한다. 이 때, router 를 inject 한다.
  * @type {Vue}
@@ -6,6 +8,10 @@
 const app = new Vue({
     el: '#app',
     router: router,
+
+    created: function () {
+        domRemove('seo');
+    },
 
     methods: {
         aTag: function (event) {
@@ -16,11 +22,7 @@ const app = new Vue({
             uris.shift();
             var url = '/' + uris.join('/');
             console.log('url: ', url);
-                try {
-                    this.$router.push(url);
-                } catch(e) {
-                    console.log('e: ', e);
-                }
+            this.$router.push(url);
         }
     }
 });
